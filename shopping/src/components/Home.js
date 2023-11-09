e2cfd81a16e78f1d363ae5e2352d0e668341bab6
JosinGeorge10 <josingkollam@gmail.com>
2023-11-09 13:19:10 +0530

    Home Page Compleated

--- shopping/src/components/Home.js
+++ shopping/src/components/Home.js
@@ -111,3 +111,94 @@ const Home = () => {
       </View>
       <ScrollView horizontal>
         {detailData.map(item => (
+          <View
+            style={{borderWidth: 2, borderColor: 'red'}}
+            key={item.id}
+            horizontal={true}>
+            <View style={{marginTop: 80, marginLeft: 20}}>
+              <View style={{alignItems: 'center'}}>
+                <View style={{width: '100%', height: '70%'}}>
+                  <Image style={styles.imageContainer} source={item.image} />
+                </View>
+                <View>
+                  <Text style={{fontWeight: '500', fontSize: 18}}>
+                    {item.name}
+                  </Text>
+                </View>
+                <View>
+                  <Text style={{fontWeight: '600', color: '#000'}}>
+                    ${item.price}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        ))}
+      </ScrollView>
+      <View>
+        <View>
+          <Text>Most popular</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    paddingVertical: 20,
+    backgroundColor: '#ffff',
+  },
+
+  topContainer: {
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  leftToggleContainer: {},
+  rightToggleContainer: {
+    flexDirection: 'row',
+    // justifyContent: 'space-between',
+  },
+  cartContainer: {
+    marginLeft: 10,
+  },
+
+  allDetails: {
+    alignItems: 'center',
+    padding: 5,
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 5,
+    marginRight: 20,
+    marginTop: 12,
+  },
+  allDetailsTwo: {
+    alignItems: 'center',
+    padding: 5,
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 5,
+    marginRight: 20,
+    backgroundColor: '#000',
+    marginTop: 12,
+  },
+  detailText: {
+    fontWeight: 'bold',
+    color: '#000',
+    fontSize: 14,
+  },
+  detailText2: {
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  imageContainer: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    // flex: 1,
+  },
+});
